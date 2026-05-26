42dbb446c289ea0974fc004d19ab3fd8c818b74c
Draqo <darioruamelendez@gmail.com>
2026-05-26 08:12:10 -0500

    metodos_ldoble

--- ImpTDAListaDobleEnlazada/src/tda/ListaDobleEnlazada.java
+++ ImpTDAListaDobleEnlazada/src/tda/ListaDobleEnlazada.java
@@ -4,11 +4,13 @@ public class ListaDobleEnlazada<T> {
     //ATRIBUTOS
     private NodoDoble<T> cabeza;
     private NodoDoble<T> ultimo;
+    private int cantidad;
     
     //CONSTRUCTOR
     public ListaDobleEnlazada() {
         cabeza = null;
         ultimo = null;
+        cantidad = 0;
     }
     
     //OPERACIONES
@@ -19,6 +21,7 @@ public class ListaDobleEnlazada<T> {
     //agregarAlFinal: agrega elemento al final de la lista
     public void agregarAlFinal(T item) {
         NodoDoble<T> nuevoNodo = new NodoDoble(item, null, null);
+        cantidad++;
 
         if (estaVacia()) {
             cabeza = nuevoNodo;
@@ -46,6 +49,65 @@ public class ListaDobleEnlazada<T> {
         while (aux!=null) {
             System.out.print(aux.getItem()+"\t");
             aux = aux.getAntNodo();
+        }
+    }
+    //longitud: muestra la cantidad de elementos del arreglo
+    public int longitud() {
+        return cantidad;
+    }
+    //insertarListaVacia
+    public void insertarListaVacia(T item) {
+        NodoDoble<T> nuevoNodo = new NodoDoble<>(item, null, null);
+        if (estaVacia()) {
+            cabeza = nuevoNodo;
+            ultimo = nuevoNodo;
+            cantidad++;
+        } else {
+            System.err.println("La lista no se encuentra vacia");
+        }
+    }
+    //insertarAlInicio: Inserta en una lista un elemento al inicio
+    public void insertarAlInicio(T item) {
+        NodoDoble<T> nuevoNodo = new NodoDoble<>(item, cabeza, null);
+        cantidad++;
+        if (estaVacia()) insertarListaVacia(item);
+        else {
+            cabeza.setAntNodo(nuevoNodo);   
+            cabeza = nuevoNodo;
+        }
+    }
+    //insertarAlFinal: Inserta en una lista un elemento al final
+    public void insertarAlFinal(T item) {
+        NodoDoble<T> nuevoNodo = new NodoDoble<>(item, null, ultimo);
+        cantidad++;
+        if (estaVacia()) insertarListaVacia(item);
+        else {
+            ultimo.setSgtNodo(nuevoNodo);
+            ultimo = nuevoNodo;
+        }
+    }
+    //intertarMedio: Inserta en una poscion media
+    public void insertar(T item, int posicion) {
+        cantidad++;
+        if (estaVacia() || posicion <= 0) insertarAlInicio(item);
+        else {
+            //Posicionar puntero en la posicion anterior indicada
+            NodoDoble<T> aux = cabeza;
+            int indice = 1;
+            while (indice < posicion - 1 && aux.getSgtNodo()!=null) {
+                aux = aux.getSgtNodo();
+                indice++;
+            }
+            //Si se encuenra en el ultimo nodo
+            if (aux == ultimo) {
+                insertarAlFinal(item);
+                return;
+            }
+
+            //1,2,3,___,4,5,6,7
+            NodoDoble<T> nuevoNodo = new NodoDoble<>(item, aux.getSgtNodo(), aux);
+            aux.getSgtNodo().setAntNodo(nuevoNodo);
+            aux.setSgtNodo(nuevoNodo);
         }
     }
 }
--- ImpTDAListaDobleEnlazada/src/test/TestListaDobleEnlazada.java
+++ ImpTDAListaDobleEnlazada/src/test/TestListaDobleEnlazada.java
@@ -4,12 +4,19 @@ import tda.ListaDobleEnlazada;
 public class TestListaDobleEnlazada {
     public static void main(String[] args) {
         ListaDobleEnlazada<Integer> lista = new ListaDobleEnlazada<>();
+        
         lista.agregarAlFinal(1);
         lista.agregarAlFinal(2);
         lista.agregarAlFinal(3);
+        lista.agregarAlFinal(4);
 
         lista.mostrar();
         System.out.println("\n---------------------");
         lista.mostrarInvertido();
+        System.out.println("\n---------------------");
+        
+        lista.insertar(2,0);
+        lista.insertar(10,4);
+        lista.mostrar();
     }
 }
