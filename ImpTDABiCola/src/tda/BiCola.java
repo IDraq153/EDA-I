fa2c92aad812d7d7a4f97d5621a988555bef48dc
Draq153 <darioruamelendez@gmail.com>
2026-05-28 10:00:06 -0500

    ImpTDABiCola

---
+++ ImpTDABiCola/src/tda/BiCola.java
@@ -0,0 +1,61 @@
+
+package tda;
+
+import javax.management.RuntimeErrorException;
+
+public class BiCola<T> {
+    private Nodo<T> frente;
+    private Nodo<T> ultimo;
+
+    // Operacion para crear una cola
+    public BiCola() {
+        frente = null;
+        ultimo = null;
+    }
+    public boolean estaVacia() {
+        return frente == null;
+    }
+    // Encolar: Agrega un item al final de la cola
+    public void encolarAlFinal(T item) {
+        Nodo<T> nuevoNodo = new Nodo<>(item,null);
+        if (estaVacia()) {
+            frente = nuevoNodo;
+            ultimo = nuevoNodo;
+        } else {
+            ultimo.setSgteNodo(nuevoNodo);
+            ultimo = nuevoNodo;
+        }
+    }
+    //EncolarAlFrente: Agrega un item al frente de la cola
+    public void encolarAlFrente(T item) {
+        Nodo<T> nuevNodo = new  Nodo<>(item, null);
+        if (estaVacia()) {
+            frente = nuevNodo;
+            ultimo = nuevNodo;
+        } else {
+            nuevNodo.setSgteNodo(frente);
+            frente = nuevNodo;
+        }
+    }
+    // Desencolar: eliminar el item que esta al frente de la cola y devuelve ese item
+    public T desencolarFrente() {
+        if (estaVacia()) {
+            throw new RuntimeErrorException(null, "Error: la cola esta VACIA!");
+        } else {
+            T itemAux = frente.getItem();
+            frente = frente.getSgteNodo();
+            return itemAux;
+        }
+    }
+    
+    // DesencolarFinal: eliminar el item que esta al fondo de la cola y devuelve ese item
+    public T desencolarFinal() {
+        if (estaVacia()) {
+            throw new RuntimeErrorException(null, "Error: la cola esta VACIA!");
+        } else {
+            T itemAux = ultimo.getItem();
+            ultimo = ultimo.getSgteNodo();
+            return itemAux;
+        }
+    }
+}
---
+++ ImpTDABiCola/src/test/testBicola.java
@@ -0,0 +1,20 @@
+package test;
+import tda.BiCola;
+
+public class testBicola {
+    public static void main(String[] args) {
+        BiCola<Integer> bicola = new BiCola<>();
+        bicola.encolarAlFinal(8);
+        bicola.encolarAlFinal(5);
+        bicola.encolarAlFinal(4);
+
+        bicola.encolarAlFrente(1);
+        bicola.encolarAlFrente(2);
+        bicola.encolarAlFrente(3);
+        
+        while (!bicola.estaVacia()) {
+            int temp = bicola.desencolarFrente();
+            System.err.print(temp + "\t");
+        }
+    }    
+}
